63b9936ca69634217e8f3320b961dd240bcd7e8b
Kristoffer Gonzaga <29176224+Kristoffer95@users.noreply.github.com>
2024-05-06 16:01:17 +0800

    Removed Drizzle setup to move to prisma(drizzle is not production ready yet)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,12 @@
-import { getProducts } from '@/services//users';
 import Link from 'next/link';
 
 export const revalidate = 20;
 // export const dynamic = 'auto';
 
 export default async function Home() {
-  const data = await getProducts();
-
   return (
     <div>
       <h1>Home Page</h1>
-
-      <Link href='/users'>Users Page</Link>
-
-      <pre>{JSON.stringify(data, null, 2)}</pre>
     </div>
   );
 }
--- src/app/should-load/[id]/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-async function ShouldLoad({ params: { id } }: { params: { id: string } }) {
-  await new Promise((resolve) => setTimeout(resolve, 1000));
-
-  return <div>This Should Load {id}</div>;
-}
-
-export default ShouldLoad;
