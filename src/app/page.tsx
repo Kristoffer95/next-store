528b6bc3cd691aae71cfaa131f825ff3736dddf4
Kristoffer Gonzaga <29176224+Kristoffer95@users.noreply.github.com>
2024-05-10 03:19:41 +0800

    Partially implemented Credentials with Auth.js

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
-import { prisma } from '@/utils/prisma';
-
-// export const revalidate = 20;
-// export const dynamic = 'auto';
+import { auth } from '@/auth';
 
 export default async function Home() {
+  const session = await auth();
+
   return (
     <div>
       <h1>Home Page</h1>
+      <pre>{session?.user?.name}</pre>
     </div>
   );
 }
--- src/auth.ts
+++ src/auth.ts
@@ -2,99 +2,71 @@ import NextAuth, { type NextAuthConfig } from 'next-auth';
 
 import { PrismaAdapter } from '@auth/prisma-adapter';
 import { PrismaClient } from '@prisma/client';
+import type { Provider } from 'next-auth/providers';
 
 // providers
 import Github from 'next-auth/providers/github';
-// import Credentials from 'next-auth/providers/credentials';
+import Credentials from 'next-auth/providers/credentials';
 
 const prisma = new PrismaClient();
+
+const providers: Provider[] = [
+  Github,
+  Credentials({
+    credentials: { password: { label: 'Password', type: 'password' } },
+    authorize: async (creds) => {
+      let user = null;
+
+      if (creds.password !== 'pass') return null;
+
+      try {
+        user = await prisma.user.findUnique({
+          where: {
+            email: 'test@test.com',
+          },
+        });
+
+        if (!user) {
+          throw new Error('User not found.');
+        }
+      } catch (error) {
+        throw new Error('User not found.');
+      }
+
+      console.log('user');
+      console.log(user);
+
+      return user;
+    },
+  }),
+];
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
   adapter: PrismaAdapter(prisma),
   secret: process.env.AUTH_SECRET,
-  // session: {
-  //   strategy: 'jwt',
-  // },
-  providers: [
-    Github,
-    // Credentials({
-    //   credentials: { password: { label: 'Password', type: 'password' } },
-    //   async authorize(credentials) {
-    //     if (credentials.password !== 'password') return null;
-
-    //     const user = {
-    //       id: '1',
-    //       name: 'Fill Murray',
-    //       email: 'bill@fillmurray.com',
-    //       image: 'https://source.boringavatars.com/marble/120',
-    //     };
-
-    //     console.log(user);
-
-    //     return user;
-    //   },
-    // }),
-  ],
+  session: {
+    strategy: 'jwt',
+  },
+  providers,
   callbacks: {
     authorized({ request, auth }) {
-      // const { pathname } = request.nextUrl
-      // if (pathname === "/middleware-example") return !!auth
       return true;
     },
-    async session({ session, user }) {
-      // console.log('session', session);
-      // console.log('user', user);
-      session.user.id = user.id;
+    // session data
+    async session({ session, token }) {
+      const userId = token.sub as string;
+      session.user.id = userId;
       return session;
     },
-    // jwt({ token, trigger, session }) {
-    //   if (trigger === 'update') token.name = session?.user?.name;
-    //   return token;
-    // },
+    // jwt data (can transform the jwt here)
+    async jwt({ token, user }) {
+      return token;
+
+      // example: add isAdmin to the token
+      // return {
+      //   isAdmin: true
+      //   ...token,
+      // };
+    },
   },
 });
-
-// export const authOptions = {
-//   adapter: PrismaAdapter(prisma),
-//   session: {
-//     strategy: 'jwt',
-//   },
-//   providers: [
-//     // Github({
-//     //   clientId: process.env.AUTH_GITHUB_ID,
-//     //   clientSecret: process.env.AUTH_GITHUB_SECRET,
-//     // }),
-//     // Github,
-//     Credentials({
-//       name: 'Sign in',
-//       credentials: {
-//         email: {
-//           label: 'Email',
-//           type: 'email',
-//           placeholder: 'test@test/cp,',
-//         },
-//         password: { label: 'Password', type: 'password' },
-//       },
-//       async authorize(credentials) {
-//         const getUser = async (): Promise<User> =>
-//           new Promise((resolve) =>
-//             resolve({
-//               id: '1',
-//               name: 'name',
-//               email: 'test@test.com',
-//               emailVerified: null,
-//               image: 'image_link...',
-//               createdAt: new Date('2024-05-09T08:07:59.598Z'),
-//               updatedAt: new Date('2024-05-09T08:07:59.598Z'),
-//             })
-//           );
-
-//         const user = await getUser();
-
-//         return user;
-//       },
-//     }),
-//   ],
-// } satisfies NextAuthConfig;
-
-// export const handler = NextAuth(authOptions);
