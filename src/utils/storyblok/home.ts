e2e10878c75ef83bc1c7cd5058f93fb763a8b1fc
Kristoffer Gonzaga <29176224+Kristoffer95@users.noreply.github.com>
2024-05-21 17:30:52 +0800

    chore: Refactor fetchData function and initialize Storyblok API to resolve build issue from vercel

--- src/utils/storyblok/home.ts
+++ src/utils/storyblok/home.ts
@@ -1,12 +1,11 @@
 import { ISbStoryParams, getStoryblokApi } from '@storyblok/react/rsc';
+import { sbInit } from '../SbInit';
 
 export async function fetchData() {
   let sbParams: ISbStoryParams = { version: 'draft' };
 
+  sbInit();
+
   const storyblokApi = getStoryblokApi();
-  return storyblokApi.get(
-    `cdn/stories/home`,
-    sbParams
-    // { cache: 'no-store' }
-  );
+  return storyblokApi.get(`cdn/stories/home`, sbParams, { cache: 'no-store' });
 }
